b9e624131c91ebcf98011e2c5b1164deeb507915
SiddheshKanawade <siddhesh.kanawade@iitgn.ac.in>
2023-09-28 15:23:19 +0530

    skeleton

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+__version__ = "0.0.1"
+
+setup(
+    name="smart-sense-assessment2",
+    version=__version__,
+    packages=find_packages(),
+    install_requires=[
+        "",
+        ],
+)
